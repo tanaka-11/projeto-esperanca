224140f82c761e58a09ac8699797c2b78ecfa609
Marina Tanaka <huttenloucher@gmail.com>
2024-08-22 21:32:10 -0300

    Melhoria na documentação

--- Livro.java
+++ Livro.java
@@ -1,14 +1,15 @@
 public class Livro {
-    private String titulo; // Adicionando a variável 'titulo'
+    // Atributos
+    private String titulo;
     private String autor;
     private int paginas = 0;
 
-    // Métodos SET (Atribuição de dados)
+    // Métodos SET (Um método setter permite definir ou atualizar o valor de um atributo)
     // Titulo
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
-    
+
     // Autor
     public void setAutor(String autor) {
         this.autor = autor;
@@ -19,7 +20,7 @@ public class Livro {
         this.paginas = paginas;
     }
 
-    // Métodos GET (Pegando o dado atribuído)
+    // Métodos GET (Um método getter retorna o valor de um atributo específico. Por exemplo, se tivermos uma classe Livro com um atributo titulo, o método getter para esse atributo seria algo como)
     // Titulo
     public String getTitulo() {
         return this.titulo;
@@ -35,7 +36,7 @@ public class Livro {
         return this.paginas;
     }
 
-    // Método main - (Primeiro método chamado quando você executa um programa Java. Ele é obrigatório em qualquer classe que deseje ser executada como um programa independente).
+    // Método main - (Primeiro método chamado quando você executa um programa Java. Ele é obrigatório em qualquer classe que deseje ser executada como um programa independente)
     public static void main(String[] args) {
         // Criando uma instância (Objeto) da classe Livro
         Livro livro = new Livro();
@@ -45,7 +46,7 @@ public class Livro {
         livro.setAutor("Rick Riordan"); // Atribuindo o autor "Rick Riordan"
         livro.setPaginas(336); // Definindo o número de páginas como 336
         
-        // Imprimindo os valores usando os métodos getter
+        // Exibindo os valores usando os métodos getter
         // Titulo 
         System.out.println("Título: " + livro.getTitulo());
 
